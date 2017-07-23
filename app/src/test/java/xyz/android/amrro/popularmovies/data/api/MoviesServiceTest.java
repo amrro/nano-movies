18a6efc07b0b46ef6d4945167c6952fb504c5ccc
Amro Elghobary <amr.elghobary@gmail.com>
2017-07-23 03:06:42 +0200

    movie() api tested

---
+++ app/src/test/java/xyz/android/amrro/popularmovies/data/api/MoviesServiceTest.java
@@ -0,0 +1,99 @@
+package xyz.android.amrro.popularmovies.data.api;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import okio.BufferedSource;
+import okio.Okio;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import xyz.android.amrro.popularmovies.data.model.Movie;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by amrro <amr.elghobary@gmail.com> on 7/23/17.
+ */
+public class MoviesServiceTest {
+
+    private MoviesService service;
+
+    private MockWebServer server;
+
+    private Gson gson;
+
+    @Before
+    public void createServer() {
+        gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create();
+
+        server = new MockWebServer();
+        service = new Retrofit.Builder()
+                .baseUrl(server.url("/"))
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(MoviesService.class);
+    }
+
+    @After
+    public void stopService() throws IOException {
+        server.shutdown();
+    }
+
+    @Test
+    public void movie() throws Exception {
+        enqueueResponse("logan.json");
+        final Response<Movie> response = service.movie(263115).execute();
+        RecordedRequest request = server.takeRequest();
+
+        assertThat(request.getPath(), is("/movie/263115"));
+        assertThat(response.body(), notNullValue());
+        assertThat(response.body().getRevenue(), is(616218538));
+        assertThat(response.body().getVoteAverage(), is(7.5));
+        assertThat(response.body().getVoteCount(), is(4846));
+    }
+
+    @Test
+    public void credits() throws Exception {
+    }
+
+    @Test
+    public void popular() throws Exception {
+    }
+
+
+    private void enqueueResponse(String fileName) throws IOException {
+        enqueueResponse(fileName, Collections.emptyMap());
+    }
+
+    private void enqueueResponse(String fileName, Map<String, String> headers) throws IOException {
+        InputStream inputStream = getClass().getClassLoader()
+                .getResourceAsStream("responses/" + fileName);
+        BufferedSource source = Okio.buffer(Okio.source(inputStream));
+        MockResponse mockResponse = new MockResponse();
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            mockResponse.addHeader(header.getKey(), header.getValue());
+        }
+        server.enqueue(mockResponse
+                .setBody(source.readString(StandardCharsets.UTF_8)));
+    }
+
+}
